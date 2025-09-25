9da3f138480d2906ba8caa7e6f1af1c23cd70916
ChienND <>
2025-09-25 18:03:48 +0700

    Bai 20. (26) Text Inputs in SwiftUI (34)

--- LearnSwiftUI/Bai20 Text Input/TextInput.swift
+++ LearnSwiftUI/Bai20 Text Input/TextInput.swift
@@ -8,11 +8,76 @@
 import SwiftUI
 
 struct TextInput: View {
+    
+    struct User {
+        var username: String = ""
+        var password: String = ""
+    }
+    
+    @FocusState var inputFocus: Bool
+    @State var user: User = .init()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            userNameTF
+            passwordTF
+            btnSubmit
+        }
+        .padding()
+        .textFieldStyle(.roundedBorder)
+        .autocorrectionDisabled(true)
+        .textInputAutocapitalization(.never)
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                Button("Done") {
+                    inputFocus.toggle()
+                }
+            }
+        }
+        .onSubmit(of: .text, submit)
     }
 }
 
 #Preview {
     TextInput()
+}
+
+
+extension TextInput {
+    var userNameTF: some View {
+        TextField("Username", text: $user.username)
+            .textContentType(.username)
+            .focused($inputFocus)
+    }
+    
+    var passwordTF: some View {
+        SecureField("Password", text: $user.password)
+            .textContentType(.password)
+            .focused($inputFocus)
+            .submitLabel(.done)
+    }
+    
+    var btnSubmit: some View {
+        Button {
+            submit()
+        } label: {
+            Text("Submit")
+        }
+
+    }
+}
+
+private extension TextInput {
+    func submit() {
+        print("aasdfsdf \(user)")
+//        dismissKeyboard()
+        inputFocus.toggle()
+    }
+}
+
+extension View {
+    func dismissKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
