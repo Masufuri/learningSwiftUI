48fc8c5df2bfaf8d368a084328d241cafc79b268
ChienND <>
2025-09-25 14:11:14 +0700

    Bai 18. (23) SwiftUI New Button Styles And Materials in SwiftUI (31)

--- LearnSwiftUI/Bai17 ButtonsInSwiftUI/ButtonsInSwiftUI.swift
+++ LearnSwiftUI/Bai17 ButtonsInSwiftUI/ButtonsInSwiftUI.swift
@@ -10,60 +10,73 @@ import SwiftUI
 struct ButtonsInSwiftUI: View {
     @State var pressedCount: Int = 0
     var body: some View {
-        VStack(spacing: 20) {
-            VStack {
-                Text("Number of times pressed: \(pressedCount)")
-                Button {
-                    pressedCount += 1
-                } label: {
-                    Text("Example 1 Increase")
+        ScrollView {
+            VStack(spacing: 20) {
+                VStack {
+                    Text("Number of times pressed: \(pressedCount)")
+                    Button {
+                        pressedCount += 1
+                    } label: {
+                        Text("Example 1 Increase")
+                    }
                 }
-            }
-
-            VStack {
-                Text("Number of times pressed: \(pressedCount)")
-                Button {
-                    pressedCount += 2
-                } label: {
-                    Text("Example 2 Increase")
-                }
-            }
-
-            Group {
-                Button("Cancel Example Role", role: .cancel) {
-                    pressedCount += 1
-                }
-
-                Button(role: .destructive) {
-                } label: {
-                    Text("Destructive Role")
-                }
-
-                Button(role: .none) {
-                } label: {
-                    Text("Default Role")
-                }
-            }
-
-            Button {
-                print("asd")
-            } label: {
-                Text("Press me")
-                    .frame(minHeight: 44)
-                    .frame(maxWidth: .infinity)
-                    .background(.red)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .tint(.white)
-                    .font(.title)
-            }
-            .padding()
-            
-            Button {
                 
-            } label: {
-                Text("Send Mail")
+                VStack {
+                    Text("Number of times pressed: \(pressedCount)")
+                    Button {
+                        pressedCount += 2
+                    } label: {
+                        Text("Example 2 Increase")
+                    }
+                }
+                
+                Group {
+                    Button("Cancel Example Role", role: .cancel) {
+                        pressedCount += 1
+                    }
+                    
+                    Button(role: .destructive) {
+                    } label: {
+                        Text("Destructive Role")
+                    }
+                    
+                    Button(role: .none) {
+                    } label: {
+                        Text("Default Role")
+                    }
+                }
+                
+                Button {
+                    print("asd")
+                } label: {
+                    Text("Press me")
+                        .frame(minHeight: 44)
+                        .frame(maxWidth: .infinity)
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .tint(.white)
+                        .font(.title)
+                }
+                .padding()
+                
+                Button {
+                    
+                } label: {
+                    Text("Send Mail")
+                }
+                .buttonStyle(SendButtonStyle())
+                
+                Button {
+                    
+                } label: {
+                    Text("Send Mail")
+                }
+                .buttonStyle(.sendButton)
+                
+                SendButton {
+                    print("asdasd")
+                }
             }
-            .buttonStyle(SendButton())
         }
     }
 }
@@ -72,7 +85,11 @@ struct ButtonsInSwiftUI: View {
     ButtonsInSwiftUI()
 }
 
-struct SendButton: ButtonStyle {
+extension ButtonStyle where Self == SendButtonStyle {
+    static var sendButton:SendButtonStyle { .init() }
+}
+
+struct SendButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
             Image(systemName: "paperplane")
@@ -85,6 +102,17 @@ struct SendButton: ButtonStyle {
         .background(.mint)
         .clipShape(RoundedRectangle(cornerRadius: 7))
         .foregroundStyle(.white)
+        .opacity(configuration.isPressed ? 0.6 : 1)
         
+    }
+}
+
+struct SendButton: View {
+    var callback: () -> Void = { }
+    var body: some View {
+        Button(action: callback) {
+            Text("Send Mail")
+        }
+        .buttonStyle(.sendButton)
     }
 }
---
+++ LearnSwiftUI/Bai18 New Button Styles And Materials/NewButtonStylesAndMaterials.swift
@@ -0,0 +1,168 @@
+//
+//  NewButtonStylesAndMaterials.swift
+//  LearnSwiftUI
+//
+//  Created by Chien ND on 25/9/25.
+//
+
+import SwiftUI
+
+struct NewButtonStylesAndMaterials: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Button("Hello") {
+                }
+                .padding()
+                .background(.red)
+                .tint(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Button("What is this?") {
+                    
+                }
+                .tint(.red)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 8))
+                .controlSize(.large)
+            }
+            
+            VStack(spacing: 16) {
+                Text("Button Style Examples")
+                
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+                .buttonBorderShape(.roundedRectangle(radius: 8))
+                .controlSize(.large)
+                
+                Button("Button Style Examples"){}
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                
+                Button("Button Style Examples"){}
+                    .buttonStyle(.borderless)
+                    .tint(.mint)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                
+                Button("Button Style Examples"){}
+                    .buttonStyle(.plain)
+                    .tint(.mint)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                
+                Button("Button Style Examples"){}
+                    .buttonStyle(.automatic)
+                    .tint(.mint)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                
+            }
+            
+            VStack(spacing: 16) {
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+                .buttonBorderShape(.roundedRectangle(radius: 8))
+                .controlSize(.large)
+                
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.large)
+                
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+                
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+                .buttonBorderShape(.automatic)
+                .controlSize(.large)
+            }
+            
+            VStack(spacing: 16) {
+                Text("Button control Size Examples")
+                    .bold()
+                
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.mini)
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.small)
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.regular)
+                Button("Button Style Examples") {
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.large)
+            }
+            
+            VStack(spacing: 24) {
+                Text("Material Examples")
+                
+                Button("Material Examples") {
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .controlSize(.large)
+                
+                Button("Material Examples") {
+                }
+                .padding()
+                .background(.thinMaterial, in: .capsule)
+                .controlSize(.large)
+                
+                Button("Material Examples") {
+                }
+                .padding()
+                .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 8))
+                .controlSize(.large)
+                
+                Button("Material Examples") {
+                }
+                .padding()
+                .background(.thickMaterial, in: RoundedRectangle(cornerRadius: 8))
+                .controlSize(.large)
+                
+                Button("Material Examples") {
+                }
+                .padding()
+                .background(.ultraThickMaterial, in: RoundedRectangle(cornerRadius: 8))
+                .controlSize(.large)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(.mint)
+        }
+    }
+}
+
+#Preview {
+    NewButtonStylesAndMaterials()
+}
