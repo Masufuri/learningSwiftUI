ec8f62c6169094320051384203275c9f7df1eaaf
ChienND <>
2025-09-29 15:41:21 +0700

    Bai 25. (33) Background and Overlay In SwiftUI (40)

--- LearnSwiftUI/Bai25 BackgroundAndOverlay/BackgroundAndOverlay.swift
+++ LearnSwiftUI/Bai25 BackgroundAndOverlay/BackgroundAndOverlay.swift
@@ -9,7 +9,86 @@ import SwiftUI
 
 struct BackgroundAndOverlay: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("5")
+                    .padding()
+                    .font(.footnote.weight(.bold))
+                    .background(.yellow)
+                
+                Text("5")
+                    .padding()
+                    .font(.footnote.weight(.bold))
+                    .background {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundColor(.white)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                    }
+                    .background(.yellow)
+                
+                Text("5")
+                    .padding()
+                    .font(.footnote.weight(.bold))
+                    .background {
+                        Circle()
+                            .fill(.yellow.opacity(0.3))
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                    }
+                
+                Text("5")
+                    .padding()
+                    .font(.footnote.weight(.bold))
+                    .background(alignment: .bottom) {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                    }
+                
+                Image(.tasbihBall1)
+                    .resizable()
+                    .padding()
+                    .frame(width: 100, height: 100)
+                    .background {
+                        Circle()
+                            .fill(.blue)
+                    }
+                    .overlay(alignment: .bottom) {
+                        Text("Subscribe")
+                            .font(.footnote.weight(.bold))
+                            .padding(4)
+//                            .padding(.vertical, 8)
+                            .background {
+                                Capsule()
+                                    .fill(.yellow)
+                            }
+                    }
+                
+                Image(.tasbihBall1)
+                    .resizable()
+                    .padding()
+                    .frame(width: 100, height: 100)
+                    .background {
+                        Circle()
+                            .fill(.blue)
+                    }
+                    .overlay(alignment: .bottom) {
+                        Text("Subscribe")
+                            .foregroundStyle(.white)
+                            .font(.footnote.weight(.bold))
+                            .padding(4)
+//                            .padding(.vertical, 8)
+                            .background(.ultraThinMaterial, in: Capsule())
+                    }
+            }
+        }
     }
 }
 
