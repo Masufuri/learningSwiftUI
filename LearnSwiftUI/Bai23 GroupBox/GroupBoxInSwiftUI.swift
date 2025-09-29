b6947b4f5966af0de5f7bd833cb0c1f522e8bac1
ChienND <>
2025-09-29 15:40:23 +0700

    Bai 23. (30) GroupBox In SwiftUI And SwiftUI GroupBoxStyle (38)

--- LearnSwiftUI/Bai23 GroupBox/GroupBoxInSwiftUI.swift
+++ LearnSwiftUI/Bai23 GroupBox/GroupBoxInSwiftUI.swift
@@ -7,22 +7,69 @@
 
 import SwiftUI
 
-struct GroupBox: View {
+struct GroupBoxInSwiftUI: View {
     var body: some View {
         VStack {
-            groupb
+            GroupBox("Saved Items") {
+                Text("You've saved 5 items")
+            }
+            
+            GroupBox {
+                Text("You've saved 5 items")
+            } label: {
+                Label {
+                    Text("Saved Items")
+                } icon: {
+                    Image(systemName: "star")
+                        .symbolVariant(.fill)
+                }
+
+            }
+            
+            GroupBox {
+                Text("You've saved 5 items")
+            } label: {
+                Label {
+                    Text("Saved Items")
+                        .bold()
+                } icon: {
+                    Image(systemName: "star")
+                        .symbolVariant(.fill)
+                }
+
+            }.groupBoxStyle(.groupBoxStyle)
+
+
 
         }
+        .padding()
+        .background(.mint)
     }
 }
 
 #Preview {
-    GroupBox()
+    GroupBoxInSwiftUI()
 }
 
 
 struct GroupBoxStyleee: GroupBoxStyle {
     func makeBody(configuration: Configuration) -> some View {
-        <#code#>
+        VStack(alignment: .leading, spacing: 0) {
+            configuration.label
+//                .font(.system(size: 17, weight: .semibold))
+//                .frame(maxWidth: .infinity,alignment: .leading)
+            configuration.content
+                .frame(maxWidth: .infinity,alignment: .center)
+            
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(.thickMaterial, in: RoundedRectangle(cornerRadius: 8))
+    }
+}
+
+extension GroupBoxStyle where Self == GroupBoxStyleee {
+    static var groupBoxStyle:GroupBoxStyleee {
+        .init()
     }
 }
