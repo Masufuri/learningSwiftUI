d44b3d1d206273614e632fdcdb9d81b09cc2d6f2
ChienND <>
2025-09-29 15:40:52 +0700

    Bai 24. (32) iOS 15 ControlGroup In SwiftUI & ControlGroupStyle (39)

--- LearnSwiftUI/Bai24 ControlGroupInSwiftUI/ControlGroupInSwiftUI.swift
+++ LearnSwiftUI/Bai24 ControlGroupInSwiftUI/ControlGroupInSwiftUI.swift
@@ -9,10 +9,73 @@ import SwiftUI
 
 struct ControlGroupInSwiftUI: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 32) {
+                VStack {
+                    Text("Quantity")
+                    ControlGroup {
+                        //                        Text("asd")
+                        decreaseBtn
+                        increaseBtn
+                    }
+                }
+
+                VStack {
+                    Text("Quantity")
+                    ControlGroup {
+                        //                        Text("asd")
+                        decreaseBtn
+                        increaseBtn
+                    }
+                    .controlGroupStyle(.navigation)
+                }
+
+                ControlGroup {
+                    //                        Text("asd")
+                    decreaseBtn
+                    increaseBtn
+                }
+                .controlGroupStyle(.quantity)
+            }
+        }
+        .padding()
     }
 }
 
 #Preview {
     ControlGroupInSwiftUI()
+}
+
+extension ControlGroupInSwiftUI {
+    var increaseBtn: some View {
+        Button {
+        } label: {
+            Label("Increase", systemImage: "plus")
+        }
+    }
+
+    var decreaseBtn: some View {
+        Button {
+        } label: {
+            Label("Decrease", systemImage: "minus")
+        }
+    }
+}
+
+struct ControlGroupQuantityStyle: ControlGroupStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading) {
+            Label("Quantity", systemImage: "bag")
+                .font(.body.weight(.bold))
+            HStack {
+                configuration.content
+            }
+        }
+    }
+}
+
+extension ControlGroupStyle where Self == ControlGroupQuantityStyle {
+    static var quantity: ControlGroupQuantityStyle {
+        .init()
+    }
 }
