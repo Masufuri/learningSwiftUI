515899178a601db80ee0e1e122baac9569e96752
ChienND <>
2025-09-29 15:39:46 +0700

    Bai 22. (29) Adding Text in SwiftUI And TextStyles (37)

--- LearnSwiftUI/Bai22 AddingText/AddingText.swift
+++ LearnSwiftUI/Bai22 AddingText/AddingText.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct AddingText: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .bold()
+                .italic()
+                .underline(true, color: .red)
+            Text("My Text Here")
+                .foregroundStyle(.mint)
+                .font(.largeTitle.weight(.black))
+            
+            Text("My Text Here")
+                .font(.system(size: 18, weight: .semibold, design: .rounded))
+            Text(.now, style: .timer)
+            Text(.now, style: .time)
+        }
+        
+        Group {
+            Text(.now, style: .date)
+            Text(Date.now, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
+            Text(72.30, format: .currency(code: "gbp"))
+            Text(72.30, format: .currency(code: "usd"))
+            Text(Date.now...Date.now.addingTimeInterval(3600))
+            Text("View calendar \(Image(systemName: "calendar")) here")
+        }
     }
 }
 
