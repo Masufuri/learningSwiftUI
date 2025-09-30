301fbc09c08120916dafb0762132df2a65980f97
ChienND <>
2025-09-30 14:25:32 +0700

    Bai 29. (39) ScrollView in SwiftUI And Automatic Scrolling With ScrollViewReader In SwiftUI (48)

--- LearnSwiftUI/Bai29 ScrollViewInSwiftUI/ScrollViewInSwiftUI.swift
+++ LearnSwiftUI/Bai29 ScrollViewInSwiftUI/ScrollViewInSwiftUI.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct ScrollViewInSwiftUI: View {
+    @State var hasReachEnd: Bool = false
+    var range: ClosedRange<Int> = 0 ... 50
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal,showsIndicators: false) {
+                    HStack {
+                        ForEach(range, id: \.self) { item in
+                            createTxtVw(with: item)
+                        }
+                    }
+                    .padding(.bottom, 50)
+                    .overlay(alignment: hasReachEnd ? .bottomTrailing : .bottomLeading) {
+                        HStack {
+                            Button {
+                                if let firstIndex = range.min() {
+                                    withAnimation {
+                                        proxy.scrollTo(firstIndex)
+                                    }
+                                    hasReachEnd = false
+                                }
+                            } label: {
+                                Image(systemName: "arrow.backward")
+                            }
+                            
+                            Button {
+                                if let lastIndex = range.max() {
+                                    withAnimation {
+                                        proxy.scrollTo(lastIndex)
+                                    }
+                                    hasReachEnd = true
+                                }
+                            } label: {
+                                Image(systemName: "arrow.forward")
+                            }
+                        }
+                        .symbolVariant(.circle.fill)
+                        .font(.title.weight(.black))
+                    }
+                }
+                
+                
+            }
+        }
+        
+
     }
 }
 
 #Preview {
     ScrollViewInSwiftUI()
+}
+
+extension ScrollViewInSwiftUI {
+    func createTxtVw(with: Int) -> some View {
+        Text("\(with)")
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(.mint, in: RoundedRectangle(cornerRadius: 8))
+            .padding(.horizontal, 8)
+    }
 }
