d3d83d35a5cca5d48b6a9e6473003b9ca476a649
ChienND <>
2025-09-30 14:24:57 +0700

    Bai 28. (38) How To Use ForEach In SwiftUI With Identifiable (46)

--- LearnSwiftUI/Bai28 ForEachInSwiftUI/ForEachInSwiftUI.swift
+++ LearnSwiftUI/Bai28 ForEachInSwiftUI/ForEachInSwiftUI.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+struct Person28: Identifiable {
+    let id = UUID()
+    let userName: String
+    let lastName: String
+
+    static var data: [Person28] {
+        [
+            Person28(userName: "What", lastName: "Where"),
+            Person28(userName: "Alo", lastName: "Hello")
+        ]
+    }
+}
 
 struct ForEachInSwiftUI: View {
+    @State var persons: [Person28] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+//            ForEach(persons, id: \.userName) { person in
+//                Text("\(person.userName) \(person.lastName)")
+//            }
+            ForEach(persons) { person in
+//                Text("\(person.userName) \(person.lastName)")
+                PersonView(person: person)
+            }
+        }
+        .onAppear() {
+            self.persons = Person28.data
+        }
     }
 }
 
 #Preview {
     ForEachInSwiftUI()
+}
+
+
+struct PersonView: View {
+    let person: Person28
+    var body: some View {
+        Text("\(person.userName) \(person.lastName)")
+    }
 }
